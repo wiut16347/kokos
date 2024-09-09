987695aac38b95ae8e350044fce7cdbd232f10ab
abdulloh <wiut16347@gmail.com>
2024-09-09 18:56:49 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./cart.html",
+    "./search.html",
+    "./personal.html",
+    "./order.html",
+    "./brand.html",
+    "./description.html",
+    "./discount.html",
+    "./discountItem.html",
+    "./company.html",
+    "./contacts.html",
+    "./review.html",
+    "./filter.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
